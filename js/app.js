0d0c899a07caca244cea00374d9fb2dd93ec1af3
Isaac Lopes <isaacgrdlopes@gmail.com>
2021-04-09 21:29:34 -0300

    Inclusao da calculadora de adubos

--- js/app.js
+++ js/app.js
@@ -4,8 +4,38 @@ app.controller('calcMudas', function($scope){
     let areaEspacamento = 0;
 
     $scope.calcularMudas = function(data){
-        areaCanteiro = (data.largura * data.comprimento);
-        areaEspacamento = (data.espacColunas * data.espacLinhas);
-        $scope.totalMudas = (areaCanteiro / areaEspacamento);
+        if (data) {
+            areaCanteiro = (data.largura * data.comprimento);
+            areaEspacamento = (data.espacColunas * data.espacLinhas);
+            $scope.totalMudasPorCanteiro = (areaCanteiro / areaEspacamento);
+            $scope.totalMudas = ($scope.totalMudasPorCanteiro * data.nroCanteiros);
+            $scope.totalMudasPerda = ($scope.totalMudas + ($scope.totalMudas * 0.1));
+            $scope.message = "";
+        } else {
+            $scope.message = "Erro preencha os campos!";
+        }
+    }
+
+    $scope.limpar = function () {
+        delete $scope.data;
+        delete $scope.totalMudasPorCanteiro;
+        delete $scope.totalMudas;
+        delete $scope.totalMudasPerda;
+    }
+});
+
+app.controller('calcAdubo', function($scope){
+    $scope.options = ["N", "P", "K"];
+    let hectarMetrosQuadrado = 10000;
+
+    $scope.calcular = function (data) {
+        $scope.valorNutricional =  ((data.recoNutriente * data.areaParcela) / hectarMetrosQuadrado);
+        $scope.qtdeAduboParcela = (((data.doseAdubo * $scope.valorNutricional) / data.pctNutriente) * 1000);
+    }
+
+    $scope.limpar = function () {
+        delete $scope.data;
+        delete $scope.valorNutricional;
+        delete $scope.qtdeAduboParcela;
     }
 });
